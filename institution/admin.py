04c05af8a50bfd0171038996ae1707651d30b65e
Krzysztof Sikora <krzyssikora@gmail.com>
2026-08-29 00:55:31 +0200

    feat(public-pages): add PublicPage and the controller-identity fields

--- institution/admin.py
+++ institution/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from institution.models import BrandColor
 from institution.models import Institution
+from institution.models import PublicPage
 
 
 class BrandColorInline(admin.TabularInline):
@@ -12,3 +13,9 @@ class BrandColorInline(admin.TabularInline):
 @admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
     inlines = [BrandColorInline]
+
+
+@admin.register(PublicPage)
+class PublicPageAdmin(admin.ModelAdmin):
+    list_display = ("slug", "language", "updated_at")
+    list_filter = ("slug", "language")
